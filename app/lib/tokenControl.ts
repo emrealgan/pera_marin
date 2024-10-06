47b5f7d3104d4d48c952c1820bcabbfdf04e8ebe
emrealgan <alganemre.8@gmail.com>
2024-10-06 12:10:37 +0300

    updte

--- app/lib/tokenControl.ts
+++
@@ -1,9 +0,0 @@
-import { getToken } from "next-auth/jwt";
-import { NextRequest, NextResponse } from "next/server";
-
-export async function tokenControl(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-  if (!token) {
-    return new NextResponse("Forbidden", { status: 403 });
-  }
-}
